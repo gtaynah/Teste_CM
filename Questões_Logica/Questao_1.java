5fc881e8accf6a09f0dbe6ee1d0c403ec01155f2
gtaynah <gtaynah@gmail.com>
2023-07-26 18:47:45 -0300

    Add files via upload

---
+++ Questões_Logica/Questao_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Questao_1{
+	public static void main(String[] args) throws Exception {
+		Scanner ler = new Scanner(System.in);
+		int n1, n2;
+		System.out.printf("Insira um número: ");
+  		n1 = ler.nextInt();
+		System.out.printf("Insira outro número: ");
+  		n2 = ler.nextInt();
+		if(n1>n2){
+			System.out.println("A raiz quadrada de " + n1 + " é " + Math.sqrt(n1));
+			System.out.println("O quadrado de " + n2 + " é " + Math.pow(n2,2));
+		}
+		else{
+			System.out.println("A raiz quadrada de " + n2 + " é " + Math.sqrt(n2));
+			System.out.println("O quadrado de " + n1 + " é " + Math.pow(n1,2));
+		}
+
+	}
+	
+}
---
+++ Questões_Logica/Questao_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Questao_2{
+	public static void main(String[] args) throws Exception {
+		Scanner ler = new Scanner(System.in);
+		int ano;
+		System.out.printf("Insira seu ano de nascimento: ");
+  		ano = ler.nextInt();
+		int resto = ano % 4;
+		if (resto==0){
+			System.out.println("O ano " + ano + " é bissexto");
+		}
+		else{
+			System.out.println("O ano " + ano + " não é bissexto");
+		}
+		
+
+	}
+	
+}
